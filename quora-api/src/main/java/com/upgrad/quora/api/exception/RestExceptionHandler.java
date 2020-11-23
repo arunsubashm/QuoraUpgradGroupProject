31190db82d1069c3d22d4c19f04bb0cd32aa9091
Rajkumar Lolla <rajkumarlollaupgrad@github.com>
2020-11-23 12:10:46 +0530

    Handled AuthenticationFailedException in the Quora-API --> RestExceptionHandler to throw access forbidden exception for bad user name or Password.

--- quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.upgrad.quora.api.exception;
 
 import com.upgrad.quora.api.model.ErrorResponse;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,18 @@ public class RestExceptionHandler {
     @ExceptionHandler(SignUpRestrictedException.class)
     public ResponseEntity<ErrorResponse> signUpRestrictedException(SignUpRestrictedException exception, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(exception.getCode()).message(exception.getErrorMessage()), HttpStatus.CONFLICT);
+    }
+
+    /** Handles the AutenticationFailedException. This is triggered when user failed to authenticate himself. Bad user name or Password.
+     * AuthenticationFailedException - HttpStatus.UNAUTHORIZED
+     *
+     * @param authFailedException
+     * @param request WebRequest
+     * @return ResponseEntity
+     */
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<ErrorResponse> AuthenticationFailedException(AuthenticationFailedException authFailedException, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(authFailedException.getCode()).message(authFailedException.getErrorMessage()), HttpStatus.UNAUTHORIZED);
     }
 }

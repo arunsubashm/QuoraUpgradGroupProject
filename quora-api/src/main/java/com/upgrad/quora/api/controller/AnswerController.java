2c29ef9ccb60893272cdb4c47b00c5d2b970b34f
Arun Subash Manickam <arunsubashm@gmail.com>
2020-11-28 16:04:59 +0530

    Create Answer API Endpoint Implementation

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -1,19 +1,18 @@
 package com.upgrad.quora.api.controller;
 
-import com.upgrad.quora.api.model.AnswerDeleteResponse;
+import com.upgrad.quora.api.model.*;
 import com.upgrad.quora.service.business.AnswerService;
 import com.upgrad.quora.service.business.AuthenticationService;
 import com.upgrad.quora.service.entity.AnswerEntity;
-import com.upgrad.quora.service.entity.UserAuthTokenEntity;
-import com.upgrad.quora.service.exception.AnswerNotFoundException;
-import com.upgrad.quora.service.exception.AuthorizationFailedException;
-import com.upgrad.quora.service.exception.RequestViolationException;
-import com.upgrad.quora.service.exception.UserNotFoundException;
+import com.upgrad.quora.service.exception.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/")
@@ -39,5 +38,28 @@ public class AnswerController {
         AnswerEntity answerEntity = answerService.deleteQuestion(authorization, answerId);
         final AnswerDeleteResponse answerDeleted = new AnswerDeleteResponse().id(answerEntity.getUuid()).status("ANSWER DELETED");
         return new ResponseEntity<AnswerDeleteResponse>(answerDeleted, HttpStatus.OK);
+    }
+
+    /**This endpoint is implemented to create new answer /question/{questionId}/answer/create in Quora
+     *
+     * @param accessToken, Question ID
+     * @param answerRequest
+     * @return ResponseEntity <AnswerResponse></AnswerResponse>
+     * @throws AuthorizationFailedException, RequestViolationException
+     */
+    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerResponse> createAnswer(@PathVariable("questionId") final String id, @RequestHeader("authorization") final String accessToken,
+                                                       final AnswerRequest answerRequest) throws AuthorizationFailedException, InvalidQuestionException {
+
+        AnswerEntity answerEntity = new AnswerEntity();
+
+        answerEntity.setAnswer(answerRequest.getAnswer());
+        answerEntity.setDate(ZonedDateTime.now());
+        answerEntity.setUuid(UUID.randomUUID().toString());
+
+        answerService.createAnswer(accessToken, id, answerEntity);
+
+        AnswerResponse answerResponse = new AnswerResponse().id(answerEntity.getUuid()).status("ANSWER CREATED");
+        return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.CREATED);
     }
 }
